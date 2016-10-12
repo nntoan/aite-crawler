37539f3fca1a1221db9b19e952e1bccba04b662d
Toan Nguyen <destro.nnt@gmail.com>
2016-10-12 16:23:23 +0700

    Added save stream code to scraper.

--- aite_crawler.py
+++ aite_crawler.py
@@ -1,13 +1,37 @@
-from lxml import etree, html
-from requests.adapters import HTTPAdapter
+#!/usr/bin/env python
+
 import requests
 import os
 import json
 import re
+from lxml import etree, html
+from requests.adapters import HTTPAdapter
+
+__author__ = 'Toan Nguyen'
+__version__ = '1.0.0'
+
+title = """
+                 ___  ___  __   __   __        __    __   __        
+ /\  |    |    |  |  |__  |__) /  \ /  \ |__/ /__`  /  ` /  \  |\/| 
+/~~\ |___ |___ |  |  |___ |__) \__/ \__/ |  \ .__/ .\__, \__/  |  | 
+                                                                    
+      ___  __      __   __   __        __   ___  __                 
+|  | |__  |__)    /__` /  ` |__)  /\  |__) |__  |__)                
+|/\| |___ |__)    .__/ \__, |  \ /~~\ |    |___ |  \                
+                                                                    
+                      By : Toan Nguyen
+          me @ nntoan.com
+          Version : 1.0.0
+A tool to download all ebook's available on
+www.allitebooks.com and save them in current
+directory.
+"""
 
 aite_sm = 'http://www.allitebooks.com/post-sitemap{0}.xml'
 
 urls = []
+
+print(title)
 
 with open('list-of-post-urls-allitbooks.md', 'w') as fp:
     for i in range(1, 8):
@@ -18,7 +42,7 @@ with open('list-of-post-urls-allitbooks.md', 'w') as fp:
             url = sitemap.getchildren()[0].text
             fp.write('[{0}]({1})\n'.format(url, url))
             urls.append(url)
-        print('Done with ' + str(i))
+        print('Post Sitemap ' + str(i) + ' is done')
 
 down_dict = {}
 
@@ -28,18 +52,30 @@ if not os.path.exists('all-it-ebooks'):
 save_dir = os.path.abspath(os.path.join(os.curdir, 'all-it-ebooks'))
 
 print('Crawling in Progress ......\n')
-for i, url in enumerate(urls[1:101]):
-    print(i)
+for i, url in enumerate(urls[1:]):
     page = requests.Session()
     page.mount(url, HTTPAdapter(max_retries=5))
     page = page.get(url)
-    tree = html.fromstring(page.content)
+    tree = html.fromstring(page.content)    
     down_link = tree.xpath("//*[@class=\"download-links\"]/a/@href")
     file_name = down_link[0].split('/')[-1]
-    title = re.sub('[^A-Za-z0-9]+', '-', file_name.split('.')[0])    
-    down_dict[title] = down_link[0]
-    
-print('\nAll Urls have been crawled.')
+    title = re.sub('[^A-Za-z0-9]+', '-', file_name.split('.')[0])        
+    down_dict[title] = down_link[0]    
+    save_loc = os.path.join(save_dir, file_name)
+
+    #Starting to download the files
+    data = requests.get(down_link[0], stream=True)
+    if not os.path.exists(save_loc):
+        print('\nNow writing {0} - {1}'.format(i + 1, file_name))
+        with open(save_loc, 'wb') as f:
+            print(url)            
+            for chunk in data.iter_content(chunk_size=4096):
+                if chunk:
+                    f.write(chunk)
+    else:
+        print('\nFile Exists. Skipped {0} - {1}'.format(i + 1, file_name))
+
+print('\nAll Urls have been crawled and saved.')
 print('\nWriting links to JSON file.\n')
 
 with open('all-it-ebooks-download-links.json', 'w') as fp:
